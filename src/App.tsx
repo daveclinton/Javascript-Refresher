bf2ac2fa8483cc8f7f837c34d27e4052c764fdf0
daveclinton <clintondavid46@gmail.com>
2022-10-11 23:20:22 +0300

    feature: Add the navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello Navbar</h1>
-        <h2>This is where the magic happens</h2>
-      </header>
+    <div>
+      <Navbar />
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello Navbar</h1>
+          <h2>This is where the magic happens</h2>
+        </header>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return <nav>Navigation</nav>;
+}
+
+export default Navbar;
